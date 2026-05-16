f3773bd532a7d453eef4731b0bb127596c99da25
brnlimaa <bernardoolima07@gmail.com>
2026-05-16 16:05:36 -0300

    atualização

---
+++ frontend/lib/screens/login_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context){
+
+    return Scaffold(
+
+      appBar: AppBar(
+          title: const Text(
+              'FocusPath'
+          )
+      ),
+
+      body: const Center(
+
+        child: Text(
+            'Tela Login'
+        ),
+
+      ),
+
+    );
+
+  }
+
+}
